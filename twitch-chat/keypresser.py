325e9a549885c74a0c121b96c222b4d043f55436
AndrewSB <asbreckenridge@me.com>
2014-09-21 10:39:53 -0400

    change keystroke bindings to 'l' and 'r'

--- twitch-chat/keypresser.py
+++ twitch-chat/keypresser.py
@@ -1,18 +1,7 @@
-# import win32com.client as comclt
- 
-# class Keypresser:
- 
-#     wsh = None;
- 
-#     def __init__(self):
-#         self.wsh = comclt.Dispatch("WScript.Shell");
-#     def key_press(self, key):
-#         self.wsh.SendKeys(key)
-
-
-import os
+import os 
+class Keypresser:
 # minimize active window
 
-def key_press(key):
-	os.system("osascript -e 'tell application \"Chrome\" to activate'")
-	os.system("osascript -e 'tell application \"System Events\" to keystroke \""+ key +"\"'")
+	def key_press(key):
+		os.system("osascript -e 'tell application \"Chrome\" to activate'")
+		os.system("osascript -e 'tell application \"System Events\" to keystroke \""+ key +"\"'")
